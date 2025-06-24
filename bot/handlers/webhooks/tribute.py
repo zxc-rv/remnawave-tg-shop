7a90e24427fb4a65146193896125d569ad1781df
Machka Pasla <161734431+machka-pasla@users.noreply.github.com>
2025-06-24 19:12:17 +0300

    Move tribute webhook route to service

--- bot/handlers/webhooks/tribute.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-from bot.services.tribute_service import TributeService
-
-
-async def tribute_webhook_route(request: web.Request):
-    tribute_service: TributeService = request.app['tribute_service']
-    raw_body = await request.read()
-    signature_header = request.headers.get('trbt-signature')
-    return await tribute_service.handle_webhook(raw_body, signature_header)
